6a9d875357d747cb975f07fec6cdabec066fd3b1
Ben <benjaminstacey87@gmail.com>
2022-11-21 10:03:51 +1300

    moving files and imports

---
+++ jamming/src/Components/App/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Playlist from './Playlist.html'
+import SearchBar from './SearchBar.html'
+import SearchResults from './SearchResults.html'
+import App from './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div class="App">
+         
+          <div class="App-playlist">
+            
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
